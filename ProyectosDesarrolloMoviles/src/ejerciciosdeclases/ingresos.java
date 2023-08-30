b3482f0202b2497dcab0043c28c333c6335a2684
garroyve <arrovelez123@gmail.com>
2023-08-30 12:00:59 -0500

    cargar datos
    nuvos

--- ProyectosDesarrolloMoviles/src/ejerciciosdeclases/ingresos.java
+++ ProyectosDesarrolloMoviles/src/ejerciciosdeclases/ingresos.java
@@ -9,24 +9,42 @@ public class ingresos {
     }
     public static void Ingresos () {
         Scanner sc = new Scanner(System.in);
-        ArrayList<Double> valores = new ArrayList<Double>();
-        double suma = 0;
+        ArrayList<Double> ingresos = new ArrayList<Double>();
+        ArrayList<Double> egresos = new ArrayList<Double>();
+        double  suma = 0;
+        double suma2 = 0;
         int contador = 0;
-        System.out.println("cuantos valores quieres guardar");
+        System.out.println("cuantos ingresos quieres guardar");
         int val = sc.nextInt();
 
         while (contador < val){
-            System.out.println("ingresar valores");
+            System.out.println("Ingresos");
             double valor = sc.nextDouble();
-            valores.add(valor);
+            if (valor > 0){
+                ingresos.add(valor);
+            }else{
+                egresos.add(valor);
+            }
+
             contador++;
 
         }
-        for (int i = 0; i < valores.size(); i++){
-            suma += valores.get(i);
+        for (int i = 0; i < ingresos.size(); i++){
+            suma += ingresos.get(i);
 
         }
-        System.out.println(suma);
+        System.out.println("los ingresos son: " + suma);
+
+        for (int i = 0; i < egresos.size(); i++){
+            suma2 += egresos.get(i);
+
+        }
+        System.out.println("los egresos son: " + suma2);
+
+        double suma4 = (suma+suma2);
+        System.out.println(suma4);
+        System.out.println("el balance es : " + suma4);
 
     }
+
 }
